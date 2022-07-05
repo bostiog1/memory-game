10ffd7c5d8f660c4716013b8b0e167209d90d9d8
Teo <teo.bostiog@yahoo.com>
2022-07-05 19:54:35 +0300

    s

--- app.js
+++ app.js
@@ -49,6 +49,28 @@ const cardArray = [
     }
 ]
 
-console.log(cardArray);
+cardArray.sort(() => 0.5 - Math.random()) // compara 2 valori si le sorteaza
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard() {
+    for(let i = 0; i < cardArray.length; i++){
+        const card = document.createElement('img')
+        card.setAttribute('src', 'image/blank.pnk')
+        card.setAttribute('data-id', i)
+        gridDisplay.append(card)
+    }
+}
+
+createBoard()
+
+console.log(grid)
+
+
+
+
+
+
+
 
 
